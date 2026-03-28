2ab475792e09ca447d4fe34adb5ee8760fc72325
yhj <dbsguswl4117@naver.com>
2026-03-29 01:15:05 +0900

    fix: img 경로 수정

--- next.config.ts
+++ next.config.ts
@@ -4,16 +4,16 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: '**.notion.so',
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8090',
+        pathname: '/uploads/**',
       },
       {
         protocol: 'https',
-        hostname: '**.s3.amazonaws.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.amazonaws.com',
+        hostname: 'localhost',
+        port: '8090',
+        pathname: '/uploads/**',
       },
     ],
   },
